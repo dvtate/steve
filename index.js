84152bd45af75715872449119bc5c788072c0c20
Dustin Van Tate Testa <toast27@gmail.com>
2017-06-23 09:39:22 -0400

    getting started

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const tg = new Telegram.Telegram('api key')
+
+class PingController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    pingHandler($) {
+        $.sendMessage('pong')
+    }
+
+    get routes() {
+        return {
+            'pingCommand': 'pingHandler'
+        }
+    }
+}
+
+
+tg.router
+    .when(
+        new TextCommand('ping', 'pingCommand'),
+        new PingController()
+    )
