16d517eaa680e9b73ff61125623f4ef3ac3249bd
Dustin Van Tate Testa <toast27@gmail.com>
2017-06-23 21:25:49 -0400

    restructuring

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-'use strict'
+'use strict';
 
-const Telegram = require('telegram-node-bot')
-const TelegramBaseController = Telegram.TelegramBaseController
-const TextCommand = Telegram.TextCommand
-const tg = new Telegram.Telegram('api key')
+const Telegram = require('telegram-node-bot');
+const TelegramBaseController = Telegram.TelegramBaseController;
+const TextCommand = Telegram.TextCommand;
+const tg = new Telegram.Telegram('api key');
 
-class PingController extends TelegramBaseController {
-    /**
-     * @param {Scope} $
-     */
-    pingHandler($) {
-        $.sendMessage('pong')
-    }
 
-    get routes() {
-        return {
-            'pingCommand': 'pingHandler'
-        }
-    }
+function loadCommands() {
+	const dir = './commands/';
+	const fs = require('fs');
+
+	fs.readdir(dir, (err, files) => {
+		files.forEach(file => {
+			const cmd = require(dir + file);
+			tg.router.when(
+				new TextCommand(cmd.command, cmd.command),
+				new cmd.controller()
+			);
+		});
+	});
 }
 
-
-tg.router
-    .when(
-        new TextCommand('ping', 'pingCommand'),
-        new PingController()
-    )
+loadCommands();
