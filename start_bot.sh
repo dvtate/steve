e2eafd3e684a8f0f3567c359235c2dba9f0accbc
Dustin Van Tate Testa <toast27@gmail.com>
2017-06-26 09:09:21 -0400

    comments and stuff

--- start_bot.sh
+++ start_bot.sh
@@ -10,8 +10,14 @@ function cleanup {
 	printf "\nSteve - Goodbye\n"
 }
 
-
+# run before starting
 init
+
+# run cleanup before exiting
 trap cleanup EXIT
+
+# start steve
 TELEGRAM_TOKEN=123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11 node index.js
-echo "error
+
+# this should never get run
+echo "error"
