0bfacb774f8fd39a154b79d89ad156c84889c09e
Dustin Van Tate Testa <toast27@gmail.com>
2017-06-26 09:05:40 -0400

    more salable solution

--- start_bot.sh
+++ start_bot.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
-TELEGRAM_TOKEN= (insert telegram bot token here)
-node index.js
+
+# gets run before Steve is started
+function init {
+	printf "Steve - Hello\n"
+}
+
+# gets run after Steve is ended
+function cleanup {
+	printf "\nSteve - Goodbye\n"
+}
+
+
+init
+trap cleanup EXIT
+TELEGRAM_TOKEN=123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11 node index.js
+echo "error
