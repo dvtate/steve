b3efe871837b8be9b1f1d66eb882a51487497642
Dustin Van Tate Testa <toast27@gmail.com>
2017-07-04 18:14:07 -0400

    fix dependencies

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 ## This script automatically sets everything up for you so
 ## to be run upon cloneing the repo
 
-chmod +x update.sh steve.sh
+## make everything runable
+chmod +x setup.sh update.sh steve.sh
 
 # get bot token
 printf "Enter your Telegram Bot API token: "
@@ -13,4 +14,4 @@ read token
 sed -i "s/^export TELEGRAM_TOKEN=.*/export TELEGRAM_TOKEN=${token}/" steve.sh
 
 # install dependencies
-npm install --save package.json
+npm install --save node-telegram-bot-api
