87ef6981a540c2f851acb997068c60ab7e550219
Dustin Van Tate Testa <toast27@gmail.com>
2017-07-05 01:10:32 -0400

    accept preset tokens

--- setup.sh
+++ setup.sh
@@ -6,12 +6,15 @@
 ## make everything runable
 chmod +x setup.sh update.sh steve.sh
 
-# get bot token
-printf "Enter your Telegram Bot API token: "
-read token
+# if token was set as a result of update.sh
+if [ -z "$TELEGRAM_TOKEN" ]; then
+	# get bot token
+	printf "Enter your Telegram Bot API token: "
+	read TELEGRAM_TOKEN
+fi
 
 # put token into steve.sh
-sed -i "s/^export TELEGRAM_TOKEN=.*/export TELEGRAM_TOKEN=${token}/" steve.sh
+sed -i "s/^export TELEGRAM_TOKEN=.*/export TELEGRAM_TOKEN=${TELEGRAM_TOKEN}/" steve.sh
 
 # install dependencies
 npm install --save node-telegram-bot-api
