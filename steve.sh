6da98dbb61beb8782b2e75ab788eeb7f384e8a5a
Dustin Van Tate Testa <toast27@gmail.com>
2017-07-05 02:36:52 -0400

    make the node a sub-process of steve.sh

--- steve.sh
+++ steve.sh
@@ -4,12 +4,13 @@
 ## in addition it helps to maintain privacy of our bot token
 
 # telegram bot token (this will be edited by setup.sh)
-export TELEGRAM_TOKEN=[your telegram bot token]
+export TELEGRAM_TOKEN=406827913:AAFB6_g45axPoH15us0dnAVdTIhkBVd7nZg
 
 
 # gets run after Steve is killed
 function cleanup {
 	printf "\nSteve - Goodbye\n"
+	kill $STEVE_PID
 }
 
 # run cleanup before exiting
@@ -18,8 +19,12 @@ trap cleanup EXIT
 # gets run before Steve is started
 echo "Steve - Hello"
 
-# start steve
-node index.js
+# start steve & mark his PID
+node index.js & 
+STEVE_PID=$!
+
+# wait for eternity... unless they kill me...
+cat
 
 # this should never get run
 echo "error in  steve.sh -1"
