8dc07a0b7c37f360c26440aa947c7277650504c0
Dustin Van Tate Testa <toast27@gmail.com>
2017-07-05 01:12:15 -0400

    now functional and doesn't need admin to enter key

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@
 ## this is used to update the bot to the newest version from github
 ## the goal here is to minimize downtime
 
+echo ""
 
 # move old version of steve to another directory
 printf "backing up old version of steve..."
@@ -13,22 +14,31 @@ printf " done\n"
 
 # clone the newest version of steve from github
 echo "cloning the newest version of steve..."
-cd ".."
-git clone https://github.com/robobibb/robobibb-steve-bot
+cd "../"
+git clone "https://github.com/robobibb/robobibb-steve-bot"
 
-# setup new steve
+
+
+# get bot token from the old steve.sh
+printf "retrieving token from old steve... "
+# Explanation: from the file steve.sh; on the line which has "export TELEGRAM_TOKEN=";
+#   set token = to the text on the right side of the "="
+export TELEGRAM_TOKEN=$(cat "steve_old/steve.sh" | grep "export TELEGRAM_TOKEN=" | awk -F"=" '{print $2}')
+printf "done\n"
+
+# setup new Steve
 cd robobibb-steve-bot
 chmod +x setup.sh steve.sh update.sh
 ./setup.sh
 
-# stop any currently running instance of the bot
+# Kill current steve
 printf "killing current instance of Steve..."
 
-# these could make unintended victims
-pkill -f steve.sh
-pkill -f node index.js
+# NOTE: these could make unintended victims...
+pkill -f "steve.sh"
+pkill -f "node index.js"
 printf " done\n"
 
-# restart bot
+# make a new steve as fast as possible to minimize downtime
 echo "reviving Steve..."
 ./steve.sh
