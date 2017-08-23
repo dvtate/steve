abbf14503213b9978d8d5c158a73a6c4547c7176
Dustin Van Tate Testa <toast27@gmail.com>
2017-08-23 19:00:08 -0400

    more good quotes less bad ones

--- fortune.js
+++ fortune.js
@@ -6,8 +6,9 @@ module.exports.getText = function() {
 		"So tell me, where I should go?\nto the left where nothing is right...\nor to the right where nothing is left...",
 		"Look at you, hacker, a pathetic creature of meat and bone. How can you challenge a perfect immortal machine?",
 		"How beautiful it is to do nothing and then rest afterward.",
-		"A computer lets you make more mistakes faster than any other invention, with the possible exceptions of handguns and Tequilla.\n-- Mitch Ratcliffe",
-		"Think before sharing with others."
+		"With my luck, I'll probably be reincarnated as me.",
+		"Think before sharing with others.",
+		"More good code has been written in languages denounced as \"bad\" than in languages proclaimed \"wonderful\" -- much more.\n-- Bjarne Stroustrup"
 	];
 
 	return fortunes[Math.floor(Math.random() * fortunes.length)];
