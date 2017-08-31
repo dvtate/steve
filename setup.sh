a8db35ce934939458799520869dfc298018b836d
Dustin Van Tate Testa <toast27@gmail.com>
2017-08-31 16:44:18 -0400

    new depends

--- setup.sh
+++ setup.sh
@@ -34,5 +34,5 @@ printf "done\n"
 
 # install dependencies
 echo "installing dependencies..."
-npm install --save node-telegram-bot-api
+npm install --save node-telegram-bot-api github
 echo "installed dependencies"
