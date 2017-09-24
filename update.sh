ecc16db51326ed19ef05b8900b85d5b2b0bce0a7
Dustin Van Tate Testa <toast27@gmail.com>
2017-09-23 22:10:25 -0400

    different approach hopefully eliminating probs

--- update.sh
+++ update.sh
@@ -6,38 +6,27 @@
 echo "Steve is redefining himself... updating sources from github"
 echo "There should be zero noticeable down time..."
 
-# move old version of steve to another directory
-printf "\nBacking up old version of Steve... "
 
-curname=$(basename $(pwd))
-cd ..
-mv "$curname" "steve_old/"
-
-printf "done\n"
-
-# clone the newest version of steve from github
-echo "Cloning the newest version of Steve... "
-git clone "https://github.com/robobibb/robobibb-steve-bot"
 
 # get bot token from the old steve.sh
-printf "retrieving token from the old steve... "
+printf "retrieving Telegram token from Steve... "
+
 # Explanation: from the file steve.sh; on the line which has "export TELEGRAM_TOKEN=";
 #   set token = to the text on the right side of the "="
-export TELEGRAM_TOKEN=$(cat "steve_old/steve.sh" | grep "export TELEGRAM_TOKEN=" | awk -F"=" '{print $2}')
+export TELEGRAM_TOKEN=$(cat "steve.sh" | grep "export TELEGRAM_TOKEN=" | awk -F"=" '{print $2}')
 printf "done\n"
+
+
+# clone the newest version of steve from github
+printf "Fetching the newest version of Steve..."
+git fetch --all
+printf "Hard forcing changes to Steve's source..."
+git reset --hard origin/master
+
 
 # setup new Steve
 echo "Re-configuring Steve similar to old one..."
-cd "robobibb-steve-bot"
-chmod +x setup.sh steve.sh update.sh
 sh "setup.sh"
 
-# Kill current steve
-printf "Killing current instance of Steve..."
-killall "steve.sh"
-printf " done\n"
-
-# make a new steve as fast as possible to minimize downtime
-echo "Reviving Steve..."
-sh "steve.sh"
-
+# kill and revive steve
+printf "respawning steve..." && sys "killall steve.sh && ./steve.sh"
