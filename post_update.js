49d92adf12d80e172153ee6f982577ca325b032f
Dustin Van Tate Testa <toast27@gmail.com>
2017-10-20 15:00:45 -0400

    function for posting updates onto our site
    robobibb.github.io

---
+++ post_update.js
@@ -0,0 +1,29 @@
+module.exports.MakeUpdate = function (msg, match, token) {
+
+
+	if (msg.reply_to_message.document.file_size > 50000000)
+		bot.sendMessage(msg.chat.from,
+			"error: upload greater than 50mb, try putting videos and other large files on external sites. \
+If you need help feel free to contact @ridderhoff",
+			{ reply_to_message : msg.message_id });
+
+	const download = require("download");
+
+	bot.getFileLink(msg.reply_to_message.document.file_id).then(fileURL => {
+		console.log(fileURL);
+		download(fileURL).then(data => {
+	    		fs.writeFileSync(file, data);
+
+
+		}).catch(error => {
+			bot.sendMessage(msg.chat.id, "error: file not found", {
+				reply_to_message : msg.message_id
+			});
+		});
+
+	}).catch(error => {
+		bot.sendMessage(msg.chat.id, "error: failed to create link", {
+			reply_to_message : msg.message_id
+		});
+	});
+}
