e31e87d7ed99cf0a55416a0a6d3ffae05587ad73
Dustin Van Tate Testa <toast27@gmail.com>
2018-04-25 11:43:54 -0400

    maximum bug density
    how tf did such a tiny amount of code have so many fucking bugs???

--- index.js
+++ index.js
@@ -525,10 +525,10 @@ bot.onText(/^\/flip(?:@robobibb_bot)? (.+)/, (msg, match) => {
 	logCmd(msg, "/flip'ed some text")
 });
 
-bot.onText(/\/shrug(?:@robobibb_bot)?(?:$|\s)/, msg => {
+bot.onText(/(?:^|\s)\/shrug(?:@robobibb_bot)?(?:$|\s)/, msg => {
 	const replyOptions = [
 		msg => {
-			bot.sendMessage(msg.chat.id, "¯\_(ツ)_/¯", { reply_to_message_id : msg.message_id });
+			bot.sendMessage(msg.chat.id, "¯\\_(ツ)_/¯", { reply_to_message_id : msg.message_id });
 		},
 		msg => {
 			bot.sendSticker(msg.chat.id, "CAADBQADvwIAApGL8gchVHFwk3j6iQI", {
